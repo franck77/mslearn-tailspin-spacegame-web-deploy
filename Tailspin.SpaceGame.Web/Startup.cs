b8261694af5b4667e36f76cda4009fabbf8ace50
Franck Vieira <franck.vieira@keyrus.com>
2019-11-05 16:52:07 +0100

    Add database schema checks to the pipeline

--- Tailspin.SpaceGame.Web/Startup.cs
+++ Tailspin.SpaceGame.Web/Startup.cs
@@ -40,6 +40,7 @@ namespace TailSpin.SpaceGame.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
             }
             else
             {
